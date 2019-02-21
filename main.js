441bc8e2d12325f6faf1433ff403da693e9ccacc
SongoMen <pat.kozlowski2000@onet.pl>
2019-02-21 01:16:46 +0100

    dsa

--- main.js
+++ main.js
@@ -6,15 +6,19 @@ function getIcons() {
         snapshot.forEach(function(child){ 
           let name = child.val().name;
           let url = child.val().url;
-          let div = document.createElement('div');
-          icons.appendChild(div)
-          div.className="icons__box";
-          div.innerHTML=`<img alt=${name} src=${url}></img><p>${name}</p>`
+          let a = document.createElement('a');
+          icons.appendChild(a)
+          a.href=`${url}`
+          a.download =`${name}`
+          a.target="blank"
+          a.className="icons__box";
+          a.innerHTML=`<li><img src=${url}></img><p>${name}</p></li>`
         });
       })
       .then(function(){
           setTimeout(() => {
              document.getElementById('loader').style.display='none'
+             document.getElementById('container').style.display='flex'
              document.getElementById('container').style.animation='show 1s'
           }, 1000);
       })
@@ -23,14 +27,15 @@ function getIcons() {
   function filterFunc(){
     let li,a,txtValue,filter;
     filter = input.value;
+    a = icons.getElementsByTagName('a');
     li = icons.getElementsByTagName('li');
     for (i = 0; i < li.length; i++) {
         p = li[i].getElementsByTagName("p")[0];
         txtValue = p.textContent || p.innerText;
         if (txtValue.indexOf(filter) > -1) {
-          li[i].style.display = "flex";
+          a[i].style.display = "block";
         } else {
-          li[i].style.display = "none";
+          a[i].style.display = "none";
         }
       }
   }
